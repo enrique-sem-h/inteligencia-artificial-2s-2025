bfa402fba5bebd6a94ee6d717cf7a7f5483b6981
enrique-sem-h <carvalhoenrique33@gmail.com>
2025-11-25 18:03:30 -0300

    first push setup

---
+++ Project_Hercules/Project_Hercules/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project_Hercules
+//
+//  Created by Enrique Carvalho on 25/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project_Hercules/Project_Hercules/Project_HerculesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_HerculesApp.swift
+//  Project_Hercules
+//
+//  Created by Enrique Carvalho on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_HerculesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
