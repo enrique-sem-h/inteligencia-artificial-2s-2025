d20af21a09d240aafef7b8ff764229718ec32d34
victorinacio@gmail.com <Victor.Inacio+bec@bloomenergy.com>
2025-11-25 19:02:54 -0300

    Xcode

---
+++ projeto-hercules/projeto-hercules/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  projeto-hercules
+//
+//  Created by ChustrupFx on 25/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
